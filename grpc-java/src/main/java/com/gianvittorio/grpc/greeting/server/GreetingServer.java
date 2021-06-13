ca2933321e01e31bfe82690e4b69fcc9bf4521ca
gianvittorio <gianvittorio.castellano@gmail.com>
2021-06-13 20:59:32 -0300

    dummy server client

---
+++ grpc-java/src/main/java/com/gianvittorio/grpc/greeting/server/GreetingServer.java
@@ -0,0 +1,29 @@
+package com.gianvittorio.grpc.greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello gRPC");
+
+        Server server = ServerBuilder.forPort(50052)
+                .build();
+
+        server.start();
+
+        Runtime.getRuntime()
+                .addShutdownHook(new Thread(() -> {
+                    System.out.println("Received Shutdown Request");
+
+                    server.shutdown();
+
+                    System.out.println("Successfully stopped the server");
+                }));
+
+        server.awaitTermination();
+    }
+}
